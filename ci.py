acf32e43b8f3b113cd74fa156c15166473062b97
Zrexer <122536817+Zrexer@users.noreply.github.com>
2024-01-16 21:14:57 +0330

    Create ci.py

---
+++ ci.py
@@ -0,0 +1,27 @@
+import requests 
+import bs4 
+
+respone = requests.get("https://www.askapache.com/online-tools/http-headers-tool/")
+
+soup = bs4.BeautifulSoup(respone.text, "html.parser")
+
+name = []
+x = {}
+val = []
+
+for i in soup.find_all("input"):
+    d = str(i).split("<input")
+    for x2 in d:
+        x3 = str(x2).split()
+        
+        for b in x3:
+            if b.startswith("name"):
+                namex = b.replace("name=", "")
+                name.append(namex)
+            
+            if b.startswith("value"):
+                vx = b.replace("value=", "")
+                val.append(vx)
+
+for h in range(len(name)):
+    print(f"Name: {name[h]} => {val[h]}")
